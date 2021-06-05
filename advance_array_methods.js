ed06728575f5bb7391edb4732a94dc66444b6658
Leanbow1708 <ma8469514210@gmail.com>
2021-06-05 11:44:26 +0530

    advance array methods

---
+++ advance_array_methods.js
@@ -0,0 +1,11 @@
+function square(num){
+    return num*num
+}
+let arr = [1,2,3,4]
+// 1**************
+// let arr2 = arr.map(square)
+// console.log(arr2)
+// let arr2 = arr.map(function(n,idx){ 
+//     console.log(idx) //this will print index
+//     return n*n
+// })
